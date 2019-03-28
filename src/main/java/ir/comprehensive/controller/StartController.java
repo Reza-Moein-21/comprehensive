b3e416267cd2ae0e48d8b27c3c0010b115a7655a
reza_moein <rezamoeinpe@yahoo.com>
2019-03-29 01:29:00 +0430

    Change top bar button
    -Use svg path for header button image in css

--- src/main/java/ir/comprehensive/controller/StartController.java
+++ src/main/java/ir/comprehensive/controller/StartController.java
@@ -1,11 +1,6 @@
 package ir.comprehensive.controller;
 
-import com.jfoenix.controls.JFXDrawer;
-import com.jfoenix.controls.JFXHamburger;
-import com.jfoenix.transitions.hamburger.HamburgerSlideCloseTransition;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.input.MouseEvent;
 import org.springframework.stereotype.Controller;
 
 import java.net.URL;
@@ -14,29 +9,9 @@ import java.util.ResourceBundle;
 @Controller
 public class StartController implements Initializable {
 
-    @FXML
-    JFXHamburger appHamburger;
-
-    @FXML
-    JFXDrawer appDrawer;
-
-    HamburgerSlideCloseTransition slideCloseTransition;
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        slideCloseTransition = new HamburgerSlideCloseTransition(appHamburger);
-        slideCloseTransition.setRate(-1);
-        appHamburger.addEventHandler(MouseEvent.MOUSE_PRESSED, this::appHamburgerMousePress);
+
     }
 
-    private void appHamburgerMousePress(MouseEvent mouseEvent) {
-        slideCloseTransition.setRate(slideCloseTransition.getRate() * -1);
-        slideCloseTransition.play();
-
-        if (appDrawer.isOpened()) {
-            appDrawer.close();
-        } else {
-            appDrawer.open();
-        }
-    }
 }
