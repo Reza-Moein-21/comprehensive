e1194c05b7e1997d9ae96eb0434d6e27df5e8808
reza_moein <rezamoeinpe@yahoo.com>
2019-10-31 00:00:04 +0330

    Add welcome page...

--- src/main/java/ir/comprehensive/Start.java
+++ src/main/java/ir/comprehensive/Start.java
@@ -2,17 +2,26 @@ package ir.comprehensive;
 
 import ir.comprehensive.utils.MessageUtils;
 import ir.comprehensive.utils.ScreenUtils;
+import javafx.animation.FadeTransition;
+import javafx.animation.ParallelTransition;
 import javafx.application.Application;
+import javafx.concurrent.Task;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import java.io.IOException;
 
 import static ir.comprehensive.utils.MessageUtils.getMessageBundle;
 
@@ -28,24 +37,73 @@ public class Start extends Application {
 
     @Override
     public void init() {
-        springContext = SpringApplication.run(Start.class);
         Font.loadFont(getClass().getResource("/fonts/shabnam.ttf").toExternalForm(), ScreenUtils.getActualSize(32));
         fxmlLoader = new FXMLLoader();
-        fxmlLoader.setControllerFactory(springContext::getBean);
         fxmlLoader.setResources(getMessageBundle());
+        fxmlLoader.setLocation(getClass().getResource("/fxml/start.fxml"));
     }
 
+    private Stage getWelcomeStage() {
+        Stage stage = new Stage();
+        stage.initStyle(StageStyle.TRANSPARENT);
+        StackPane vBox = new StackPane();
+        vBox.setMaxHeight(10);
+        vBox.setMaxWidth(10);
+        Label t = new Label("Welcome page");
+        vBox.getChildren().addAll(t);
+        applyFontStyle(vBox);
+        t.setStyle("-fx-font-size: " + ScreenUtils.getActualSize(100));
+
+        Scene startScene = new Scene(vBox, 2000, 399);
+        startScene.setFill(Color.TRANSPARENT);
+
+        stage.setScene(startScene);
+        stage.show();
+
+        FadeTransition ft = new FadeTransition(Duration.millis(1000));
+        ft.setFromValue(0.1);
+        ft.setToValue(1);
+        ft.setAutoReverse(false);
+
+        ParallelTransition pt = new ParallelTransition(vBox, ft);
+        pt.play();
+        return stage;
+    }
     @Override
     public void start(Stage primaryStage) throws Exception {
-        fxmlLoader.setLocation(getClass().getResource("/fxml/start.fxml"));
-        Pane rootNode = fxmlLoader.load();
-        applyFontStyle(rootNode);
-        Scene scene = new Scene(rootNode, ScreenUtils.getActualSize(3200), ScreenUtils.getActualSize(1800));
-        primaryStage.setScene(scene);
-        primaryStage.getIcons().add(new Image("/image/icon2.png"));
-        primaryStage.setOnCloseRequest(e -> System.exit(0));
-        primaryStage.setTitle(MessageUtils.Message.APP_TITLE);
-        primaryStage.show();
+        Stage stage = getWelcomeStage();
+
+
+        final Task<ConfigurableApplicationContext> progressTask = new Task<ConfigurableApplicationContext>() {
+            @Override
+            public ConfigurableApplicationContext call() {
+                springContext = SpringApplication.run(Start.class);
+                return springContext;
+            }
+        };
+
+        progressTask.setOnSucceeded(event -> {
+            fxmlLoader.setControllerFactory(springContext::getBean);
+            Pane rootNode = null;
+            try {
+                rootNode = fxmlLoader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            applyFontStyle(rootNode);
+            Scene scene = new Scene(rootNode, ScreenUtils.getActualSize(3200), ScreenUtils.getActualSize(1800));
+            primaryStage.setScene(scene);
+            primaryStage.getIcons().add(new Image("/image/icon2.png"));
+            primaryStage.setOnCloseRequest(e -> System.exit(0));
+            primaryStage.setTitle(MessageUtils.Message.APP_TITLE);
+            primaryStage.show();
+            stage.close();
+
+        });
+
+        Thread thread = new Thread(progressTask);
+        thread.setDaemon(true);
+        thread.start();
 
     }
 
