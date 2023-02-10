102b33541124ed3fffc0d9585e13f4ea81e196af
reza_moein <rezamoeinpe@gmail.com>
2023-02-10 19:49:12 +0330

    Delete unused DescribableDomainDao.java

--- database/src/main/java/ir/comprehensive/database/service/ProductDeliveryDao.java
+++ database/src/main/java/ir/comprehensive/database/service/ProductDeliveryDao.java
@@ -1,11 +1,11 @@
 package ir.comprehensive.database.service;
 
-import ir.comprehensive.database.service.base.DescribableDomainDao;
+import ir.comprehensive.database.service.base.DomainDao;
 import ir.comprehensive.domain.enums.ProductStatusEnum;
 import ir.comprehensive.domain.model.ProductDeliveryModel;
 
 import java.util.List;
 
-public interface ProductDeliveryDao extends DescribableDomainDao<ProductDeliveryModel, Long> {
+public interface ProductDeliveryDao extends DomainDao<ProductDeliveryModel, Long> {
     List<ProductDeliveryModel> findAllByStatus(ProductStatusEnum status);
 }
